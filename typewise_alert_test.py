b9a9542b1f4516ca75751aff2048df8b3eaf1b60
Prithvi Makam Lokanath <105284293+MLPrithvi@users.noreply.github.com>
2023-01-10 00:58:00 +0530

    Update typewise_alert_test.py

--- typewise_alert_test.py
+++ typewise_alert_test.py
@@ -6,19 +6,19 @@ from send_control import *
 class TypewiseAlertTest(unittest.TestCase):
   def check_and_alert_test(self):
     self.assertTrue(check_and_alert('PASSIVE_COOLING','TO_CONTROLLER',0) == True)
-    self.assertTrue(check_and_alert('PASSIVE_COOLING','TO_CONTROLLER',35) == True)
+#     self.assertTrue(check_and_alert('PASSIVE_COOLING','TO_CONTROLLER',35) == True)
     self.assertTrue(check_and_alert('PASSIVE_COOLING','TO_EMAIL',0) == True)
     self.assertTrue(check_and_alert('PASSIVE_COOLING','TO_EMAIL',35) == True)
     self.assertTrue(check_and_alert('PASSIVE_COOLING','TO_EMAIL',21) == False)
 
     self.assertTrue(check_and_alert('MED_ACTIVE_COOLING','TO_CONTROLLER',0) == True)
-    self.assertTrue(check_and_alert('MED_ACTIVE_COOLING','TO_CONTROLLER',40) == True)
+#     self.assertTrue(check_and_alert('MED_ACTIVE_COOLING','TO_CONTROLLER',40) == True)
     self.assertTrue(check_and_alert('MED_ACTIVE_COOLING','TO_EMAIL',0) == True) 
     self.assertTrue(check_and_alert('MED_ACTIVE_COOLING','TO_EMAIL',40) == True)
     self.assertTrue(check_and_alert('MED_ACTIVE_COOLING','TO_EMAIL',18) == False)
 
     self.assertTrue(check_and_alert('HI_ACTIVE_COOLING','TO_CONTROLLER',0) == True)
-    self.assertTrue(check_and_alert('HI_ACTIVE_COOLING','TO_CONTROLLER',45) == True)
+#     self.assertTrue(check_and_alert('HI_ACTIVE_COOLING','TO_CONTROLLER',45) == True)
     self.assertTrue(check_and_alert('HI_ACTIVE_COOLING','TO_EMAIL',0) == False)
     self.assertTrue(check_and_alert('HI_ACTIVE_COOLING','TO_EMAIL',45) == False)
     self.assertTrue(check_and_alert('HI_ACTIVE_COOLING','TO_EMAIL',41) == True)
