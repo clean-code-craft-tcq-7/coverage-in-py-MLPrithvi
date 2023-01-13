7ae55c77db9f6244396e1e3beb98369a3e090031
Prithvi Makam Lokanath <105284293+MLPrithvi@users.noreply.github.com>
2023-01-13 13:55:46 +0530

    Update printer.py

--- printer.py
+++ printer.py
@@ -1,6 +1,6 @@
 def print_send_to_controller(header,breachType):
     print(f'{header}, {breachType}')
 
-def print_send_to_email(recepient,text):
+def print_send_to_email(recepient,variable):
     print(f'To: {recepient}')
-    print("Hi, the temperature is " + text)
+    print("Hi, the temperature is " + variable)
